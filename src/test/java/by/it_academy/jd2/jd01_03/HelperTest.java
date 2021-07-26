9f85917f7799f279fdcefa29b4e62ed6fce4af4d
VadzimKrukouski <vadzimkrukouski@gmail.com>
2021-07-26 23:37:18 +0300

    Maven part4

--- src/test/java/by/it_academy/jd2/jd01_03/HelperTest.java
+++ src/test/java/by/it_academy/jd2/jd01_03/HelperTest.java
@@ -42,13 +42,31 @@ public class HelperTest {
     @MethodSource("mulProvider")
     @DisplayName("mul matrix and vector")
     public void mul(double[][] matrix, double[] vector, double[] resultArray) {
-        Assertions.assertArrayEquals(resultArray, Helper.mul(matrix, vector),1e-10);
+        Assertions.assertArrayEquals(resultArray, Helper.mul(matrix, vector), 1e-10);
     }
 
     public static Stream<Arguments> mulProvider() {
         return Stream.of(
                 Arguments.arguments(new double[][]{{8.7, 7.6}, {5.3, 4.6}, {6.7, 1.6}}, new double[]{4.6, 3.8}, new double[]{68.90, 41.86, 36.90})
 
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("mulProvider2")
+    @DisplayName("mul matrix and matrix")
+    public void mul(double[][] matrix1, double[][] matrix2, double[][] resultMatrix) {
+        Assertions.assertArrayEquals(resultMatrix, Helper.mul(matrix1, matrix2));
+    }
+
+    public static Stream<Arguments> mulProvider2() {
+        return Stream.of(
+                Arguments.arguments(new double[][]{{1.0, 2.0, 3.0}, {4.0, 5.0, 6.0}, {7.0, 8.0, 9.0}},
+                        new double[][]{{9.0, 8.0, 7.0}, {6.0, 5.0, 4.0}, {3.0, 2.0, 1.0}},
+                        new double[][]{{30, 24, 18}, {84, 69, 54}, {138, 114, 90}}),
+                Arguments.arguments(new double[][]{{4.0, 2.0, 3.0}, {4.0, 5.0, 6.0}, {7.0, 8.0, 9.0}},
+                        new double[][]{{9.0, 8.0, 7.0}, {6.0, 5.0, 4.0}},
+                        null)
         );
     }
 
