2f348988a5e23279805f3d1de24d9e7e00efa2e4
VadzimKrukouski <vadzimkrukouski@gmail.com>
2021-07-30 00:11:33 +0300

    Tomcat part1

---
+++ src/main/java/by/it_academy/jd2/HelloServlet.java
@@ -0,0 +1,16 @@
+package by.it_academy.jd2;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("text/html");
+        PrintWriter writer = resp.getWriter();
+        writer.write("<p><span style='color: blue;'>Hello, world!</span></p>");
+    }
+}
