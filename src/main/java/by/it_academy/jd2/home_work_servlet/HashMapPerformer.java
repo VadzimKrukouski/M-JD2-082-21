2122d71f7da53270655c7a41fafe0096434cc9e5
VadzimKrukouski <vadzimkrukouski@gmail.com>
2021-08-04 00:30:09 +0300

    Servlet Homework part3

---
+++ src/main/java/by/it_academy/jd2/home_work_servlet/HashMapPerformer.java
@@ -0,0 +1,18 @@
+package by.it_academy.jd2.home_work_servlet;
+
+import java.util.HashMap;
+
+public class HashMapPerformer {
+    public static HashMap<String, Integer> performerMap = new HashMap<>();
+
+    static {
+        performerMap.put("Linkin Park", 0);
+        performerMap.put("Eminem", 0);
+        performerMap.put("50 cent", 0);
+        performerMap.put("Limp Bizkit", 0);
+    }
+
+    public HashMap<String, Integer> getPerformerMap() {
+        return performerMap;
+    }
+}
--- src/main/java/by/it_academy/jd2/home_work_servlet/VoteServlet.java
+++ src/main/java/by/it_academy/jd2/home_work_servlet/VoteServlet.java
@@ -7,24 +7,42 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Arrays;
+import java.util.Map;
+import java.util.Set;
 
-@WebServlet (name = "VoteServlet", urlPatterns = "/vote")
+@WebServlet(name = "VoteServlet", urlPatterns = "/vote")
 public class VoteServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/html");
+        resp.setContentType("text/html; charset=UTF-8");
         PrintWriter writer = resp.getWriter();
 
         String performer = req.getParameter("performer");
-        String[] genre=req.getParameterValues("genre");
+        String[] genre = req.getParameterValues("genre");
         String text = req.getParameter("text");
 
-        writer.write("<p>Performer: " + performer + "</p>");
-        writer.write("<p><b>" + "Genre" + "</b></p>");
-        for (String singleGenre : genre) {
-            writer.write("<li>" + singleGenre + "</li>");
+        HashMapPerformer hashMapPerformer = new HashMapPerformer();
+        Set<String> stringSet = hashMapPerformer.getPerformerMap().keySet();
+
+        if (genre.length < 3) {
+            writer.write("<h2>" + "ERROR:Вы выбрали мало жанров. Пожалуйста переголосуйте!" + "</h2>");
+        } else {
+            for (String s : stringSet) {
+                if(s.equals(performer)){
+                    Integer integer = hashMapPerformer.getPerformerMap().get(s);
+                    integer=integer+1;
+                    hashMapPerformer.getPerformerMap().put(s,integer);
+                }
+
+            }
+            Set<Map.Entry<String, Integer>> entries = hashMapPerformer.getPerformerMap().entrySet();
+
+            writer.write("<p>Performer: " + performer + "</p>");
+            writer.write("<p><b>" + "Genre" + "</b></p>");
+            for (String singleGenre : genre) {
+                writer.write("<li>" + singleGenre + "</li>");
+            }
+            writer.write("<p>" + text + "</p>");
         }
-        writer.write("<p>" + text + "</p>");
     }
 }
