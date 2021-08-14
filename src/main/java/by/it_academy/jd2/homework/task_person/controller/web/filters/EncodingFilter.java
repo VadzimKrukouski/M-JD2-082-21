caa38e9267ebc3eaf4319115abeabdfc14ab6a83
VadzimKrukouski <vadzimkrukouski@gmail.com>
2021-08-14 08:18:13 +0300

    Homework addFilter, Listener1

--- src/main/java/by/it_academy/jd2/homework/task_person/controller/web/filters/EncodingFilter.java
+++ src/main/java/by/it_academy/jd2/homework/task_person/controller/web/filters/EncodingFilter.java
@@ -4,18 +4,21 @@ import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import java.io.IOException;
 
-@WebFilter (servletNames = "DataPersonServlet")
+//@WebFilter (servletNames = "DataPersonServlet")
 public class EncodingFilter implements Filter {
+
+    private String encoding = null;
+
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-
+        encoding = filterConfig.getInitParameter("encoding");
     }
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        response.setContentType("text/html; charset=UTF-8");
-        request.setCharacterEncoding("utf-8");
-        chain.doFilter(request,response);
+        response.setContentType("text/html; charset=" + encoding);
+        request.setCharacterEncoding(encoding);
+        chain.doFilter(request, response);
     }
 
     @Override
